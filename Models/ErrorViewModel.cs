6d33f229a698e7691364e835b796bab1b81eea5f
Pana Vlad <92287983+VladPana2001@users.noreply.github.com>
2024-05-30 12:29:52 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Music_Library_Management_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
