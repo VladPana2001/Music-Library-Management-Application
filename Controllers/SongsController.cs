902c6c8d502348b4b3b29113518bf2f8720e192a
Pana Vlad <92287983+VladPana2001@users.noreply.github.com>
2024-06-01 11:45:18 +0300

    created songs controller

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Music_Library_Management_Application.Controllers
+{
+    public class SongsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
