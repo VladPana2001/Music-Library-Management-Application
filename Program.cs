e3f273b6b1338de227698f20699dbb300b79c878
Pana Vlad <92287983+VladPana2001@users.noreply.github.com>
2024-06-07 22:43:11 +0300

    Reapply "separated controller logic to song service"
    This reverts commit 275a3229f097367b73372b4d8286421a8f866c2f.

--- Program.cs
+++ Program.cs
@@ -48,6 +48,8 @@ builder.Services.AddScoped<ISongRepository, SongRepository>();
 builder.Services.AddScoped<IPlaylistRepository, PlaylistRepository>();
 builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
 
+builder.Services.AddScoped<ISongService, SongService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
