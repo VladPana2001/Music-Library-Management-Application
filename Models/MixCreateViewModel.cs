8382fc49a7c6876268497522c2330a1b70d3b53b
Pana Vlad <92287983+VladPana2001@users.noreply.github.com>
2024-06-10 21:30:10 +0300

    slider for song trimming

--- Models/MixCreateViewModel.cs
+++ Models/MixCreateViewModel.cs
@@ -8,5 +8,18 @@ namespace Music_Library_Management_Application.Models
         public string MixDescription { get; set; }
         public List<Song> AllSongs { get; set; }
         public List<MixSong> MixSongs { get; set; } = new List<MixSong>();
+
+        public double ConvertToSeconds(string length)
+        {
+            var parts = length.Split(':');
+            if (parts.Length == 2)
+            {
+                if (int.TryParse(parts[0], out int minutes) && int.TryParse(parts[1], out int seconds))
+                {
+                    return minutes * 60 + seconds;
+                }
+            }
+            return 0;
+        }
     }
 }
