d496713d1a1580457c48ae954e8e322f4cc6b6fe
Pana Vlad <92287983+VladPana2001@users.noreply.github.com>
2024-06-13 02:38:17 +0300

    null on description and validation for create mix

---
+++ Migrations/20240612233642_NullOnDesc.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Music_Library_Management_Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullOnDesc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Mixes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Mixes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- Models/DbModels/MixDb.cs
+++ Models/DbModels/MixDb.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public byte[] MixFile { get; set; }
         public double LimiterThreshold { get; set; } = 0.9;
 
